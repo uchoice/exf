8571c72e3d09cf7058ba9496930178a056f05821
gyfeng <yuanfeng.guo@foxmail.com>
2017-10-14 23:50:26 +0800

    获取Action缓存

--- exf-core/src/main/java/net/uchoice/exf/core/cache/SimpleCache.java
+++ exf-core/src/main/java/net/uchoice/exf/core/cache/SimpleCache.java
@@ -12,34 +12,33 @@ public class SimpleCache {
 
     private final Map<String, ConcurrentMap<Object, Object>> caches = Maps.newHashMap();
 
-    @SuppressWarnings("unchecked")
     protected <T> T doCacheObject(String cacheName, Object key, Object obj) {
         ConcurrentMap<Object, Object> cache = caches.computeIfAbsent(cacheName, k -> Maps.newConcurrentMap());
         cache.put(key, obj);
-        return (T) obj;
+        return (T)obj;
     }
 
-    @SuppressWarnings("unchecked")
     protected <T> T getCachedEntity(String cacheName, Object key) {
-        if(key == null){
+        if (key == null) {
             return null;
         }
         ConcurrentMap<?, ?> cache = caches.get(cacheName);
-        if (null == cache)
+        if (null == cache) {
             return null;
-        return (T) cache.get(key);
+        }
+        return (T)cache.get(key);
     }
 
     protected boolean isEntitiesCached(String cacheName, Object key) {
         return null != getCachedEntity(cacheName, key);
     }
 
-    @SuppressWarnings("unchecked")
     protected <T> List<T> getAllCacheEntities(String cacheName) {
         ConcurrentMap<?, ?> cache = caches.get(cacheName);
-        if (null == cache)
+        if (null == cache) {
             return Lists.newArrayList();
-        return (List<T>) cache.entrySet().stream().map(Map.Entry::getValue)
-                .collect(Collectors.toList());
+        }
+        return (List<T>)cache.entrySet().stream().map(Map.Entry::getValue)
+            .collect(Collectors.toList());
     }
 }
--- exf-core/src/main/java/net/uchoice/exf/core/loader/cache/PluginCache.java
+++ exf-core/src/main/java/net/uchoice/exf/core/loader/cache/PluginCache.java
@@ -1,5 +1,7 @@
 package net.uchoice.exf.core.loader.cache;
 
+import java.util.List;
+
 import net.uchoice.exf.core.cache.SimpleCache;
 import net.uchoice.exf.model.action.ActionInst;
 import net.uchoice.exf.model.action.ActionSpec;
@@ -9,55 +11,59 @@ import net.uchoice.exf.model.container.ContainerSpec;
 public class PluginCache extends SimpleCache {
 
     private static final String CACHE_CONTAINER = "exf.container";
-    
+
     private static final String CACHE_CONTAINER_SPEC = "exf.container.spec";
-    
+
     private static final String CACHE_ACTION = "exf.action";
-    
+
     private static final String CACHE_ACTION_SPEC = "exf.action.spec";
 
     public void cacheContainerSpec(ContainerSpec container) {
-        if(null != container){
-        	doCacheObject(CACHE_CONTAINER_SPEC, container.getCode(), container);
+        if (null != container) {
+            doCacheObject(CACHE_CONTAINER_SPEC, container.getCode(), container);
         }
     }
-    
-    public ContainerSpec getCachedContainerSpec(String code){
-    	return getCachedEntity(CACHE_CONTAINER_SPEC, code);
+
+    public ContainerSpec getCachedContainerSpec(String code) {
+        return getCachedEntity(CACHE_CONTAINER_SPEC, code);
     }
-    
+
     public boolean isContainerSpecRegisted(String code) {
-    	return getCachedContainerSpec(code) != null;
+        return getCachedContainerSpec(code) != null;
     }
-    
+
     public void cacheContainer(ContainerInst container) {
-        if(null != container){
-        	doCacheObject(CACHE_CONTAINER, container.getUid(), container);
+        if (null != container) {
+            doCacheObject(CACHE_CONTAINER, container.getUid(), container);
         }
     }
-    
-    public ContainerSpec getCachedContainer(String uid){
-    	return getCachedEntity(CACHE_CONTAINER, uid);
+
+    public ContainerSpec getCachedContainer(String uid) {
+        return getCachedEntity(CACHE_CONTAINER, uid);
     }
-    
+
     public void cacheActionSpec(ActionSpec container) {
-        if(null != container){
-        	doCacheObject(CACHE_ACTION_SPEC, container.getCode(), container);
+        if (null != container) {
+            doCacheObject(CACHE_ACTION_SPEC, container.getCode(), container);
         }
     }
-    
-    public ActionSpec getCachedActionSpec(String code){
-    	return getCachedEntity(CACHE_ACTION_SPEC, code);
+
+    public ActionSpec getCachedActionSpec(String code) {
+        return getCachedEntity(CACHE_ACTION_SPEC, code);
     }
-    
+
+    public List<ActionSpec> getAllActionSpec() {
+        return getAllCacheEntities(CACHE_ACTION_SPEC);
+    }
+
     public void cacheAction(ActionInst container) {
-        if(null != container){
-        	doCacheObject(CACHE_ACTION, container.getUid(), container);
+        if (null != container) {
+            doCacheObject(CACHE_ACTION, container.getUid(), container);
         }
     }
-    
-    public ActionSpec getCachedAction(String uid){
-    	return getCachedEntity(CACHE_ACTION, uid);
+
+    public ActionSpec getCachedAction(String uid) {
+        return getCachedEntity(CACHE_ACTION, uid);
     }
 
 }
